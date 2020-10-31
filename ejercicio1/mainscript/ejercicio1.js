6c5afbc64f91246106687a9986693b1b0b796bdc
Henry <hsercas@gmail.com>
2020-10-31 16:32:09 -0500

    ejercicio1

---
+++ ejercicio1/mainscript/ejercicio1.js
@@ -0,0 +1,12 @@
+let nota1, nota2, nota3, notas_parciales_55, examen_final_30, trabajo_final_15, califica_final;
+nota1=3.5;
+nota2=3.1;
+nota3=4.9;
+examen_final_30=3.8;
+trabajo_final_15=4;
+notas_parciales_55=((nota1 + nota2 + nota3)/3) * 0.55;
+califica_final= notas_parciales_55 + (examen_final_30 * 0.3) + (trabajo_final_15 * 0.15); 
+console.log(califica_final);
+
+
+
