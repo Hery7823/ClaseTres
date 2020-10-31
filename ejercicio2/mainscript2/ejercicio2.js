7f3fe59d9ad4bea1f152103d78943e6bea7c6ece
Henry <hsercas@gmail.com>
2020-10-31 18:50:35 -0500

    Ejercicio 2

---
+++ ejercicio2/mainscript2/ejercicio2.js
@@ -0,0 +1,9 @@
+let socio1=50, socio2=80, socio3=170, inverTotal;
+inverTotal=socio1+socio2+socio3;
+socio1=(socio1/inverTotal)*100;
+socio2=(socio2/inverTotal)*100;
+socio3=(socio3/inverTotal)*100;
+
+console.log("inversion por acciones: accionista 1: "+socio1+" % accionista 2: "+socio2+" % acionista 3: "+socio3+" %");
+
+
