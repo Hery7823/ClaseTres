a889169386ec58df9d62e5da203f1ba459384258
Henry <hsercas@gmail.com>
2020-10-31 19:45:44 -0500

    Ejercicio 3

---
+++ ejercicio3/mainscript/ejercicio3.js
@@ -0,0 +1,6 @@
+let edaJuan=30, albert, ana, mam;
+albert=(edaJuan * 2)/3;
+ana=(edaJuan * 4)/3;
+mam= edaJuan + albert + ana;
+console.log("La edad de la mama de Juan es: "+mam+" años"+" la edad de Juan es: "+edaJuan+" la edad de Alberto es: "+albert+" la edad de Ana es: "+ana);
+
